63daff9cbc35d0af0609b30d9eed6242c0ad9807
xiami <891627097@qq.com>
2021-02-08 11:32:05 +0800

    实现服务注册与发现的eureka，然后就可以访问localhost:8761的eureka

---
+++ xiami-blog-eureka/src/main/java/com/xiami/EurekaApplication.java
@@ -0,0 +1,21 @@
+package com.xiami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Description：
+ *
+ * @version v1.0.0
+ * @author：zj
+ * @date：2021­02­08 11:21
+ */
+@SpringBootApplication
+//开启eureka注解
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}

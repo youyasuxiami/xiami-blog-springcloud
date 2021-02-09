884399140bc484b3966efa48333bb1d0713d2881
xiami <891627097@qq.com>
2021-02-09 10:59:20 +0800

    feign增加熔断器仪表盘

--- xiami-blog-springcloud-web-admin-feign/src/main/java/com/xiami/WebAdminFeignApplication.java
+++ xiami-blog-springcloud-web-admin-feign/src/main/java/com/xiami/WebAdminFeignApplication.java
@@ -3,6 +3,7 @@ package com.xiami;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -15,6 +16,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+//增加hystrix仪表盘
+@EnableHystrixDashboard
 public class WebAdminFeignApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebAdminFeignApplication.class, args);
---
+++ xiami-blog-springcloud-web-admin-feign/src/main/java/com/xiami/config/HystrixDashboardConfiguration.java
@@ -0,0 +1,28 @@
+package com.xiami.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Description：
+ *
+ * @version v1.0.0
+ * @author：zj
+ * @date：2021­02­09 10:48
+ */
+@Configuration
+public class HystrixDashboardConfiguration {
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+//servlet访问路径
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
